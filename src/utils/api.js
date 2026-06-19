e1f8df75f62ffc2f3621ded117a4f4cb66302576
Haftu <haftuabadi1994@gmail.com>
2026-06-20 01:23:34 +0300

    update API URL to production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-//export const API_BASE = 'http://192.168.1.31:5000/api';
-//export const API_BASE = 'http://10.10.3.83:5000/api';
-//export const API_BASE = 'http://192.168.56.1:5000/api';
-//export const API_BASE = 'http://192.168.1.4:5000/api';
- export const API_BASE = 'http://10.10.3.52:5000/api';
+
+export const API_BASE = 'https://gigwork-backend-m6wo.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
